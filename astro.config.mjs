36d773b42fb07d1fe71b288982bf4b4a04ff61e7
williamppmm <williamppmm@users.noreply.github.com>
2026-04-25 01:37:14 -0500

    chore: configure sitemap and vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://calzadomaleja.com',
+  site: 'https://malejacalzado.com',
   output: 'static',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      customPages: [],
+    }),
+  ],
 });
